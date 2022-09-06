5d8300af284af13ff27cf54ac30c453d8f5f08ad
Makarova Tamara <tamazoka@mail.ru>
2022-09-06 20:10:58 +0300

    добавлена Задача 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b){
+    System.Console.WriteLine("Число a является квадратом числа b");
+}
+else{
+    System.Console.WriteLine("Число a не является квадратом числа b");
+}
